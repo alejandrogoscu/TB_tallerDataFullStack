ed99b3afc47825fbd6228d377fcf456c0f61a6b5
alejandrogoscu <alejandrogoscu@gmail.com>
2025-06-17 12:40:36 +0200

    Creacion de modelo colegios

---
+++ models/Colegios.js
@@ -0,0 +1,60 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../index.js';
+
+const Colegio = sequelize.define(
+  'colegio',
+  {
+    codcen: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    regimen: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    direccion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    CP: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    municipio: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    provincia: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    telef: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    mail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    latitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    longitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'colegios',
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
+
+export default Colegio;
